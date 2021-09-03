554b812145a9f7e3d11098397ff8a756b5f94fbf
Jonathan Watson <thejmdw@gmail.com>
2021-09-03 14:52:54 -0400

    report with a list of items under 999

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .orders.completedorder import completed_order_list
 from .orders.incompleteorder import incomplete_order_list
+from .products.inexpensiveproducts import inexpensive_product_list
