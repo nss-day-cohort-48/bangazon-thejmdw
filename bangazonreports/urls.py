5f341aeac5129554e838bb1ecf29b68eea29f820
Jonathan Watson <thejmdw@gmail.com>
2021-09-03 15:01:30 -0400

    report for expensive products

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import completed_order_list, incomplete_order_list
 from .views import inexpensive_product_list
+from .views import expensive_product_list
 
 urlpatterns = [
     path('reports/completedorders', completed_order_list),
     path('reports/incompleteorders', incomplete_order_list),
     path('reports/inexpensiveproducts', inexpensive_product_list),
+    path('reports/expensiveproducts', expensive_product_list),
 ]
